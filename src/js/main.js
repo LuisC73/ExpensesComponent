8fc43e37924b71e2b0f7a972c29b7af9fb493b7f
Luis Miguel Castro Curequia <luiscastroc11@gmail.com>
2022-07-15 18:03:36 -0500

    bug solucionado

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,37 @@
-const data = fetch('./data.json')
-    .then(response => response.json())
-    .then (data => {return data})
+const data = [
+    {
+      "day": "mon",
+      "amount": 17.45
+    },
+    {
+      "day": "tue",
+      "amount": 34.91
+    },
+    {
+      "day": "wed",
+      "amount": 52.36
+    },
+    {
+      "day": "thu",
+      "amount": 31.07
+    },
+    {
+      "day": "fri",
+      "amount": 23.39
+    },
+    {
+      "day": "sat",
+      "amount": 43.28
+    },
+    {
+      "day": "sun",
+      "amount": 25.48
+    }
+  ];
 
 const containerChart = document.querySelector('.balance__spending');
 
-data.then((data) =>{
+
     data.forEach(item => {
         let chart = document.createElement('div'),
         divValue= '';
@@ -22,4 +49,3 @@ data.then((data) =>{
         containerChart.appendChild(chart);
     })
    
-})
